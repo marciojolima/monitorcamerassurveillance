ae25f568a83736593bc0a839f1c079d9bf695509
Marcio wsl2 <>
2023-10-31 00:01:47 -0300

    testes com VLC no linux em linha de comando

--- main.cpp
+++ main.cpp
@@ -1,36 +1,56 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 //#include <string>
 //#include <cstdlib>
-//#include <sstream>
+#include <sstream>
+
+typedef struct {
+    const char* cameras_names;
+    const char* windows_position;
+    const char* camera_feedso;
+} configuracao;
 
 int main() {
     // Leitura do arquivo config.cfg
-    std::ifstream configFile("conf.d/config.cfg");
-    if (!configFile.is_open()) {
-        std::cerr << "Erro: Não foi possível abrir o arquivo de configuração 'config.cfg'" << std::endl;
+    std::ifstream arquivoConfiguracao("conf.d/conf.cfg");
+
+    if (!arquivoConfiguracao.is_open()) {
+        std::cerr << "Erro: Não foi possível abrir o arquivo de configuração 'conf.cfg'" << std::endl;
         return 1;
     }
 
+    // Declaração de variáveis para armazenar configurações
+    std::string cameraA, cameraB;
+    int16_t x, y, deltaX, deltaY;
+    std::string streamCameraA;
+
+    arquivoConfiguracao >> cameraA >> cameraB;
+    arquivoConfiguracao >> x >> y >> deltaX >> deltaY;
+    arquivoConfiguracao >> streamCameraA;
+
+    std::stringstream prepararComando;
+
+    prepararComando << "vlc " <<
+        "'" << streamCameraA << "'";
+        /*" --screen-left=" << x <<
+        " --screen-top=" << y <<
+        " --screen-width=" << deltaX <<
+        " --screen-height=" << deltaY <<
+        " '" << streamCameraA << "'";
+        */
+    
+    std::string comando;
+    comando = prepararComando.str();
+
+    std::cout << comando << std::endl;
+
+    // Executa o comando para iniciar o stream
+    int result = system(comando.c_str());
+    
     /*
-    // Lê as configurações do arquivo
-    std::string screenResolution, videoStreamLink, videoPort;
-    if (configFile >> screenResolution >> videoStreamLink >> videoPort) {
-        // Constrói o comando para chamar o VLC com as configurações
-        std::stringstream command;
-        command << "vlc -I dummy " << videoStreamLink << " --screen-fps=30 --screen-width=" << screenResolution << " --screen-height=" << screenResolution << " --sout '#transcode{vcodec=h264,vb=800,scale=1,acodec=mp3,ab=128,channels=2}:std{access=udp,mux=ts,dst=:" << videoPort << "}'";
-
-        // Executa o comando para iniciar o stream
-        int result = system(command.str().c_str());
-        if (result != 0) {
-            std::cerr << "Erro ao executar o VLC" << std::endl;
-            return 1;
-        }
-
-        // Encerra a execução após o streaming
-        return 0;
-    } else {
-        std::cerr << "Erro: O arquivo de configuração está incompleto" << std::endl;
+    if (result != 0) {
+        std::cerr << "Erro ao executar o VLC" << std::endl;
         return 1;
     }
     */
